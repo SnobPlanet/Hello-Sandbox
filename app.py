de93e82bc798047b3ec3d8381aad885e749f1006
SnobPlanet <37011830+SnobPlanet@users.noreply.github.com>
2023-03-29 02:30:30 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    name = request.args.get('name', 'Bob')
-    return f"Hello, {name}!"
+    name = request.args.get('name', 'BOB')
+    return f"Bonjour, {name}!"
 
 if __name__ == "__main__":
     app.run()
